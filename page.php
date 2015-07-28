82a2d03c578b6e3e778546c69a3ededa48d0f2e2
Eli Zoller <zoller.eli@gmail.com>
2015-07-28 15:32:41 -0400

    fixes for slider styles and disabling comments for all content types

---
+++ page.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * The template for displaying all pages.
+ *
+ * This is the template that displays all pages by default.
+ *
+ * @package ThinkUpThemes
+ */
+
+get_header(); ?>
+
+			<?php while ( have_posts() ) : the_post(); ?>
+
+				<?php get_template_part( 'content', 'page' ); ?>
+
+			<?php endwhile; ?>
+
+<?php get_footer(); ?>
