34ff20a738e1b27351bc5909b0e429aac4d1e8fa
Eli Zoller <zoller.eli@gmail.com>
2015-09-10 08:27:02 -0400

    changing footer to be full width copyright and no social media icons - makes NU footer hardcoded in

---
+++ footer.php
@@ -0,0 +1,53 @@
+<?php
+/**
+ * The template for displaying the footer.
+ *
+ * Contains the closing of the #content div and all content after
+ *
+ * @package Quest
+ */
+
+/**
+ * Filter Footer container class
+ */
+$footer_container_cls = apply_filters( 'quest_footer_container_cls', 'container' );
+
+?>
+
+<?php if ( is_active_sidebar( 'footer-widget' ) ) : ?>
+	<footer class="quest-row main-footer">
+		<div class="<?php echo $footer_container_cls; ?>">
+			<div class="row">
+				<?php dynamic_sidebar( 'footer-widget' ); ?>
+			</div>
+		</div>
+	</footer>
+<?php endif; ?>
+
+<footer id="colophon" class="copyright quest-row" role="contentinfo">
+	<div class="<?php echo $footer_container_cls; ?>">
+		<div class="row">
+			<div class="col-md-12 copyright-text">
+				<?php echo apply_filters( 'quest_footer_copyright_text', quest_get_footer_copyright() ); ?>
+			</div>
+
+			<!-- <div class="col-md-6 social-icon-container clearfix"> -->
+				<!-- <ul> -->
+					<!-- <?php //quest_footer_social_icons(); ?> -->
+				<!-- </ul> -->
+			<!-- </div> -->
+
+		</div>
+		<!-- end row -->
+	</div>
+	<!-- end container -->
+</footer> <!-- end quest-row -->
+
+</div><!-- #page -->
+
+<?php wp_footer(); ?>
+
+<a href="#0" class="cd-top"><i class="fa fa-angle-up"></i></a>
+
+</body>
+</html>
