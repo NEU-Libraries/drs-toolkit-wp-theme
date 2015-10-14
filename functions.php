a60326ed2b5383143fde32b7c7f27a1272565715
Eli Zoller <zoller.eli@gmail.com>
2015-10-14 15:48:16 -0400

    actually enqueues the overrides.css file

--- functions.php
+++ functions.php
@@ -26,6 +26,7 @@
   add_action( 'wp_enqueue_scripts', 'theme_enqueue_styles' );
   function theme_enqueue_styles() {
      wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
+     wp_enqueue_style( 'override-style', get_stylesheet_directory_uri() . '/overrides.css' );
   }
 
   /*adds NU footer */
