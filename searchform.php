f4bf95a55da6af640af164f39a2e0cc7e0d4e2a4
Eli Zoller <zoller.eli@gmail.com>
2015-12-04 09:26:30 -0500

    the label was right before

--- searchform.php
+++ searchform.php
@@ -11,7 +11,7 @@
 	<fieldset>
 		<legend class="sr-only">Search</legend>
 		<div class="text">
-			<label for="drs-input" class="sr-only">Search</label>
+			<label for="q" class="sr-only">Search</label>
 			<input name="q" id="drs-input" type="text" placeholder="<?php esc_attr( _e( 'Search ...', 'quest' ) ); ?>" value="<?php echo esc_attr( get_search_query() ); ?>"/>
 			<button class="fa fa-search"><?php _e( 'Search', 'quest' ) ?></button>
 		</div>
