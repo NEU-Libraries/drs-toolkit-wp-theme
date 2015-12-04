e51ea3515fa9ee858e1c3076742874e4c64ebd81
Eli Zoller <zoller.eli@gmail.com>
2015-12-04 13:45:01 -0500

    hide page title on home page

--- header_helper.js
+++ header_helper.js
@@ -1,4 +1,5 @@
 jQuery(document).ready(function(){
+  jQuery(".home #title-container").hide();
   jQuery(".site-branding").removeClass("col-md-4").addClass("col-lg-6");
   jQuery("#site-navigation").removeClass("col-md-8").addClass("col-lg-6");
   if (Modernizr.touch) {
