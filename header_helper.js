1543ac8818015838adb5a071ebb4b23f95d5259f
Eli Zoller <zoller.eli@gmail.com>
2016-01-28 14:34:47 -0500

    fix for undefined $

--- header_helper.js
+++ header_helper.js
@@ -3,6 +3,6 @@ jQuery(document).ready(function(){
   jQuery(".site-branding").removeClass("col-md-4").addClass("col-lg-6");
   jQuery("#site-navigation").removeClass("col-md-8").addClass("col-lg-6");
   if (Modernizr.touch) {
-    $("#site-navigation .menu-item-has-children.dropdown").removeClass('menu-item-has-children').removeClass('dropdown');
+    jQuery("#site-navigation .menu-item-has-children.dropdown").removeClass('menu-item-has-children').removeClass('dropdown');
   }
 })
