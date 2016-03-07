18438f088d25083787408ddaed00f7208121f6e5
Damon Timm <damontimm@gmail.com>
2016-03-07 06:41:41 -0800

    convert tabs to spaces

--- searchform.php
+++ searchform.php
@@ -8,12 +8,12 @@
 
 ?>
 <form class="search" action="<?php echo esc_url( home_url('/search/') ); ?>" method="get">
-	<fieldset>
-		<legend class="sr-only">Search</legend>
-		<div class="text">
-			<label for="q" class="sr-only">Search</label>
-			<input name="q" id="drs-input" type="text" placeholder="<?php esc_attr( _e( 'Search ...', 'quest' ) ); ?>" value="<?php echo esc_attr( get_search_query() ); ?>"/>
-			<button class="fa fa-search"><?php _e( 'Search', 'quest' ) ?></button>
-		</div>
-	</fieldset>
+  <fieldset>
+    <legend class="sr-only">Search</legend>
+    <div class="text">
+      <label for="q" class="sr-only">Search</label>
+      <input name="q" id="drs-input" type="text" placeholder="<?php esc_attr( _e( 'Search ...', 'quest' ) ); ?>" value="<?php echo esc_attr( get_search_query() ); ?>"/>
+      <button class="fa fa-search"><?php _e( 'Search', 'quest' ) ?></button>
+    </div>
+  </fieldset>
 </form>
