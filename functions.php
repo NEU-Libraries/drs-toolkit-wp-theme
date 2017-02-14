9edb3f7e7018cbedcf8efc96519273d864c08e18
Eli Zoller <zoller.eli@gmail.com>
2017-02-14 06:55:16 -0500

    add ability to have overrides/admin.css file - this is a request from ecda

--- functions.php
+++ functions.php
@@ -123,7 +123,6 @@
   }
   add_action( 'wp_enqueue_scripts', 'theme_enqueue_styles' );
 
-
   /*adds NU footer */
   function quest_get_footer_copyright(){
     $footer = '<div class="col-md-3"><a alt="Northeastern University" class="northeastern-logo" href="http://www.northeastern.edu"><span class="sr-only">Northeastern University</span></a></div><div class="col-md-6"><ul class="nav nav-pills"><li><a href="http://myneu.neu.edu/cp/home/displaylogin" target="_blank">myNEU</a></li><li><a href="https://prod-web.neu.edu/webapp6/employeelookup/public/main.action" target="_blank">Find Faculty &amp; Staff</a></li><li><a href="http://www.northeastern.edu/neuhome/adminlinks/findaz.html" target="_blank">Find A-Z</a></li><li><a href="http://www.northeastern.edu/emergency/index.html" target="_blank">Emergency Information</a></li><li><a href="http://www.northeastern.edu/search/index.html" target="_blank">Search</a></li></ul><address>360 Huntington Ave., Boston, Massachusetts 02115 · 617.373.2000 ·  TTY 617.373.3768<br><span class="fa fa-copyright"></span> '.date('Y').' Northeastern University</address></div><div class="col-md-3"><ul class="nu-social"><li><a class="youtube" href="https://www.youtube.com/northeastern" target="_blank"><span class="sr-only">Northern University on YouTube</span><span aria-hidden="" class="fa fa-youtube"></span></a></li><li><a class="twitter" href="http://twitter.com/northeastern" target="_blank"><span class="sr-only">Northern University on Twitter</span><span aria-hidden="" class="fa fa-twitter"></span></a></li><li><a class="facebook" href="http://www.facebook.com/northeastern" target="_blank"><span class="sr-only">Northeastern on Facebook</span><span aria-hidden="" class="fa fa-facebook"></span></a></li></ul></div>';
@@ -993,3 +992,12 @@ function remove_shortcodes_filter( $content ) {
   }
   return $content;
 }
+
+/*allows for overrides admin.css*/
+function drstk_add_admin_css(){
+  if (file_exists(dirname(__FILE__) . '/overrides/admin.css')) {
+    wp_register_style('override-admin-style', get_stylesheet_directory_uri() . '/overrides/admin.css' );
+    wp_enqueue_style('override-admin-style');
+  }
+}
+add_action('admin_enqueue_scripts', 'drstk_add_admin_css', 20);
