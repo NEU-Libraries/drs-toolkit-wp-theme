6803849ff0e7720f09d9d52c305a39247c387703
Patrick Murray-John <p.murray-john@northeastern.edu>
2019-03-08 13:25:11 -0500

    replace empty title bar for scrolling

--- functions.php
+++ functions.php
@@ -212,7 +212,7 @@
 
   /* Override for ETD to hide useless distinction between search and browse */
   function quest_title_bar( $view ) {
-    
+    echo "<div id='title-container'></div>";
   }
   
   
